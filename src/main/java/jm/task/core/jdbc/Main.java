9938e5c00871ffc9f706a8607af74b270970c677
vnaang <71573980+vnaang@users.noreply.github.com>
2020-11-15 19:06:16 +0700

    Update Main.java

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -10,9 +10,26 @@ public class Main {
          String testName = "Ivan";
          String testLastName = "Ivanov";
          byte testAge = 5;
+        
+         String nameNick = "Nick";
+         String lastNameNick = "Natch";
+         byte ageNick = 30;
+ 
+         String nameJulian = "Julian";
+         String lastNameJulian = "Basiski";
+         byte ageJulian = 15;
+        
+         String nameElsa = "Elsa";
+         String lastNameElsa = "Grgoryan";
+         byte ageElsa = 45;
+
+
 
          userService.createUsersTable();
          userService.saveUser(testName, testLastName, testAge);
+         userService.saveUser(nameNick, lastNameNick, ageNick);
+         userService.saveUser(nameJulian, lastNameJulian, ageJulian)
+         userService.saveUser(nameElsa, lastNameElsa, ageElsa);
          System.out.println(userService.getAllUsers());
          userService.cleanUsersTable();
          userService.dropUsersTable();
